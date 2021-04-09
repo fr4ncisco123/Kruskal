a86c4b78029c1bc0a571c3416825df9e9e7744e5
francisco CGC <franciscotheslayer@hotmail.com>
2021-04-09 16:25:12 -0300

    Tradução

--- Kruskal.py
+++ Kruskal.py
@@ -1,14 +1,12 @@
 
 from collections import defaultdict
  
- 
- 
 class Graph:
  
     def __init__(self, vertices):
         self.V = vertices 
         self.graph = []  
-    def addEdge(self, u, v, w):
+    def addAresta(self, u, v, w):
         self.graph.append([u, v, w]) 
     def find(self, parent, i):
         if parent[i] == i:
@@ -56,18 +54,18 @@ class Graph:
                 self.union(parent, rank, x, y)
  
         minimumCost = 0
-        print ("Edges in the constructed MST")
+        print ("Contrução de arestas no MST")
         for u, v, weight in result:
             minimumCost += weight
             print("%d -- %d == %d" % (u, v, weight))
-        print("Minimum Spanning Tree" , minimumCost)
+        print("Árvore de abrangência mínima" , minimumCost)
 
 g = Graph(4)
-g.addEdge(0, 1, 10)
-g.addEdge(0, 2, 6)
-g.addEdge(0, 3, 5)
-g.addEdge(1, 3, 15)
-g.addEdge(2, 3, 4)
+g.addAresta(0, 1, 10)
+g.addAresta(0, 2, 6)
+g.addAresta(0, 3, 5)
+g.addAresta(1, 3, 15)
+g.addAresta(2, 3, 4)
  
 g.KruskalMST()
  
